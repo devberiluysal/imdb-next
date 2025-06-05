5641f1ea22168caca48cdfdcafcff7beae32d503
devberiluysal <135634086+devberiluysal@users.noreply.github.com>
2025-06-05 22:40:33 +0300

    Fix trending genre default

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Results from "@/components/Results"
 const API_KEY = process.env.API_KEY
 
 export default async function Home({searchParams}) {
-  const genre = searchParams.genre || 'fethcTrending'
+  const genre = searchParams.genre || 'fetchTrending'
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
